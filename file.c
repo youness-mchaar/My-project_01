0065b25cc8b46b4d6c3ca07061e22a7fa197628a
younessmchaar1999@gmail.com <younessmchaar1999@gmail.com>
2023-03-15 19:18:52 +0100

    HELLO

---
+++ file.c
@@ -0,0 +1,12 @@
+                                                                                       file.c                                                                                                
+#include<stdio.h>
+#include<stdlib.h>
+#include <stddef.h>
+
+int main(){
+ 
+printf("HELLO WORLD\n");
+
+return 0;
+}
+
